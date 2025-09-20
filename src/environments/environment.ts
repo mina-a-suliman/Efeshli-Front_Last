72247db207b03f20175b385d7d98d621be6031be
Ahmed Ali <ahmedalisaied23@gmail.com>
2025-09-20 21:30:21 +0300

    edit wishlist and profile menu

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -14,8 +14,9 @@ export const environment = {
   // apiUrl: 'http://localhost:5104/api',
 //     apiUrl: 'https://efreshliapi.runasp.net/api',
 
-  apiUrl: 'https://efreshliapi.runasp.net/api',
+  // apiUrl: 'https://efreshliapi.runasp.net/api',
   // apiUrl: 'https://localhost:5104/api',
+  apiUrl: 'http://efreshli.runasp.net/api',
   stripePublicKey: 'pk_test_51S7COpIZOEb85oX1aEwyJ52ZI393EuHmtfquCDPJlGb6CZb5qvEFJfRVzAfDnyAYiWia23ClO0kutpKuVczFNBSt00HXzvOPjp',
     googleClientId: '1060918648941-f25b5amq8gncf44dpsjrh8gb2rrklilr.apps.googleusercontent.com', // Add this
   facebookAppId: '776609238188831'    // Add this
