27ae5a05982c6b52a6890d1dcd5bce2938b8e62b
Mina Ashraf <76844952+mina-a-suliman@users.noreply.github.com>
2025-10-02 00:43:56 -0700

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,10 +4,10 @@ export const environment = {
   production: true,
 
 
- apiUrl: 'https://efreshli.runasp.net/api',  // Mina Host
+ // apiUrl: 'https://efreshli.runasp.net/api',  // Mina Host
 
  // apiUrl: 'http://localhost:5104/api',      // Localhost
-  // apiUrl: 'https://efreshliapi.runasp.net/api', // Aya Host
+  apiUrl: 'https://efreshliapi.runasp.net/api', // Aya Host
   stripePublicKey: 'pk_test_51S7COpIZOEb85oX1aEwyJ52ZI393EuHmtfquCDPJlGb6CZb5qvEFJfRVzAfDnyAYiWia23ClO0kutpKuVczFNBSt00HXzvOPjp',
     googleClientId: '1060918648941-f25b5amq8gncf44dpsjrh8gb2rrklilr.apps.googleusercontent.com', // Add this
   facebookAppId: '776609238188831'    // Add this
